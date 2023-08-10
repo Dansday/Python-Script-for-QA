3cdc8f60daab27a621c8b1c71c10fed23d767b41
Dansday <akbaryudhanto49@gmail.com>
2023-08-10 12:28:33 +0800

    URL Checker support QA env

---
+++ URL Checker/QA/run.py
@@ -0,0 +1,158 @@
+import requests
+import gzip
+from bs4 import BeautifulSoup
+import time
+import random
+import csv
+import os
+import signal
+import sys
+
+user_agents = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36"
+]
+
+crawled_urls = set()
+
+def get_status_code(url):
+    headers = {'User-Agent': random.choice(user_agents)}
+    try:
+        response = requests.get(url, headers=headers, timeout=30)
+        time.sleep(0.5)
+        return response.status_code
+    except requests.exceptions.RequestException as err:
+        print("RequestException error occurred:", err)
+    except requests.exceptions.HTTPError as errh:
+        print("HTTPError occurred:", errh)
+    except requests.exceptions.ConnectionError as errc:
+        print("ConnectionError occurred:", errc)
+    except requests.exceptions.Timeout as errt:
+        print("Timeout error occurred:", errt)
+    except Exception as e:
+        print("An unexpected error occurred:", e)
+
+    return None
+
+def crawl_sitemap(url, writer):
+    valid = "Valid URL"
+    invalid = "Invalid URL"
+    na = "N/A"
+    manual = "Need Manual Check"
+    headers = {'User-Agent': random.choice(user_agents)}
+
+    # Map of old URLs to their replacements
+    url_map = {
+        'https://iprice.hk/': 'https://ip-27342-iprice-hk.iprice.mx/',
+        'https://iprice.co.id/': 'https://ip-27342-iprice-id.iprice.mx/',
+        'https://iprice.my/': 'https://ip-27342-iprice-my.iprice.mx/',
+        'https://iprice.ph/': 'https://ip-27342-iprice-ph.iprice.mx/',
+        'https://iprice.sg/': 'https://ip-27342-iprice-sg.iprice.mx/',
+        'https://ipricethailand.com/': 'https://ip-27342-iprice-th.iprice.mx/',
+        'https://iprice.vn/': 'https://ip-27342-iprice-vn.iprice.mx/',
+        'https://iprice.au/': 'https://ip-27342-iprice-au.iprice.mx/'
+    }
+
+    response = requests.get(url, headers=headers, stream=True)
+    if 'Content-Disposition' in response.headers.keys():
+        decompressed_content = gzip.decompress(response.content)
+        soup = BeautifulSoup(decompressed_content, 'xml')
+    else:
+        soup = BeautifulSoup(response.content, 'xml')
+    urls = soup.find_all('loc')
+
+    for url in urls:
+        sitemap_url = url.get_text()
+        # Replace old URLs with new ones
+        for old_url, new_url in url_map.items():
+            sitemap_url = sitemap_url.replace(old_url, new_url)
+        if sitemap_url in crawled_urls:
+            print(f"Skipping URL (already crawled): {sitemap_url}")
+            continue
+        else:
+            crawled_urls.add(sitemap_url)
+
+        print(f"Crawling URL: {sitemap_url}")
+        status_code = get_status_code(sitemap_url)
+        if status_code is not None:
+            print(f"  Status: {status_code}")
+            if status_code == 200:
+                print(f"  {valid}")
+                writer.writerow([sitemap_url, status_code, valid])
+            else:
+                print(f"  {invalid}")
+                writer.writerow([sitemap_url, status_code, invalid])
+        else:
+            print(f"  {manual}")
+            writer.writerow([sitemap_url, na, manual])
+
+def crawl_sitemap_index(url, csv_file):
+    headers = {'User-Agent': random.choice(user_agents)}
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.content, 'xml')
+    sitemaps = soup.find_all('loc')
+
+    for sitemap in sitemaps:
+        sitemap_url = sitemap.get_text()
+        print(f"Crawling sitemap: {sitemap_url}")
+        if sitemap_url.endswith(".xml"):
+            crawl_sitemap(sitemap_url, csv_file)
+        else:
+            crawl_nested_sitemap(sitemap_url, csv_file)
+
+def crawl_nested_sitemap(url, csv_file):
+    headers = {'User-Agent': random.choice(user_agents)}
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    tbody = soup.find('tbody')
+    if tbody:
+        rows = tbody.find_all('tr')
+        for row in rows:
+            loc = row.find('td', class_='loc')
+            if loc:
+                child_sitemap_url = loc.find('a').get('href')
+                print(f"Crawling child sitemap: {child_sitemap_url}")
+                crawl_sitemap(child_sitemap_url, csv_file)
+
+def main():
+    csv_filename = input("Enter the CSV filename to save/load data: ")
+    csv_exists = os.path.exists(csv_filename)
+
+    with open(csv_filename, 'a', newline='') as csv_file:
+        fieldnames = ['URL', 'URL Status Code', 'Information']
+        writer = csv.writer(csv_file)
+        if not csv_exists:
+            writer.writerow(fieldnames)
+
+        def signal_handler(signal, frame):
+            print("\nScript interrupted. Saving data and exiting...")
+            sys.exit(0)
+
+        signal.signal(signal.SIGINT, signal_handler)
+
+        if csv_exists:
+            print(f"Resuming from the last crawled URL in '{csv_filename}'...")
+            with open(csv_filename, 'r') as existing_csv_file:
+                reader = csv.DictReader(existing_csv_file)
+                for row in reader:
+                    crawled_urls.add(row['URL'])
+
+        sitemap_index_url = input("Enter the sitemap index: ")
+        crawl_sitemap_index(sitemap_index_url, writer)
+
+if __name__ == "__main__":
+    main()
